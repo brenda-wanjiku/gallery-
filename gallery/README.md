3799febd9e3282771738ce70b3c9d08244504473
brenda-wanjiku <brendawanjiku@live.com>
2020-05-24 22:45:59 +0300

    Writes README

---
+++ gallery/README.md
@@ -0,0 +1,56 @@
+## brè & 97
+
+# Description
+A django-based application that allows allows a user to view various images that I have posted. The users can search for images depending on the categories laid out in the application. The user can also see images depending on the location where the image was posted and also the image name and the description.
+
+
+# By https://github.com/brenda-wanjiku/gallery-
+
+# Setup/Installation Requirements
+# Pre-requisites
+* python3.8
+* pip
+* Virtual environment(virtualenv)
+* Django 
+* PostgreSQL
+
+# Cloning and running
+* Clone the application using git clone(this copies the app onto your device). In your   terminal:
+
+$ git clone https://github.com/brenda-wanjiku/gallery-
+$ cd gallery
+
+# Creating the virtual environment
+* Use the following commands in your terminal to create virtual environment
+
+$ python3.8 -m venv --without-pip virtual
+
+$ source virtual/bin/env
+
+$ curl https://bootstrap.pypa.io/get-pip.py | python
+
+# Installing Django and other Modules
+$ python3.8 -m pip install Django==3.0.6
+
+$ python3.8 -m pip install django-bootstrap4
+
+
+# Testing the Application
+* To run the tests for the class files:
+
+$ python3.8 manage.py test photos
+
+# Technologies Used
+* Python 3.8
+* Django==3.0.6
+
+
+# Live Site
+*Can be accessed here 
+
+
+# Author's Contact
+If you need any clarifications or have feedback on this project , contact the author at brendawanjiku@gmail.com
+
+# License
+This software is Licensed under MIT license Copyright (2018) https://raw.githubusercontent.com/brenda-wanjiku/gallery-/master/LICENSE
--- gallery/photos/tests.py
+++ gallery/photos/tests.py
@@ -2,36 +2,37 @@ from django.test import TestCase
 from .models import Location, Category, Image
 
 # Create your tests here.
+
+
 class CategoryTestClass(TestCase):
-    
+
     def setUp(self) -> None:
-         # creating a new location and saving it
+        # creating a new location and saving it
         self.new_category = Category(category='nature')
         self.new_category.save_category()
 
     def tearDown(self):
         Category.objects.all().delete()
 
-    
     def test_save_location(self):
         self.new_category.save_category()
         category = Category.objects.all()
         self.assertTrue(len(category) > 0)
 
-    
     def test_delete_category(self):
         self.new_category.delete_category()
         category = Category.objects.all()
         self.assertEqual(len(category), 1)
 
-    
     def test_update_category(self):
         self.new_category.save_category()
-        updated_category = Category.update_category(self.new_category.id,'Food')
+        updated_category = Category.update_category(
+            self.new_category.id, 'Food')
         self.assertEqual(updated_category.category, 'Food')
 
+
 class LocationTestClass(TestCase):
-    
+
     def setUp(self):
         # creating a new location and saving it
         self.new_location = Location(location='Nairobi')
@@ -40,23 +41,21 @@ class LocationTestClass(TestCase):
     def tearDown(self):
         Location.objects.all().delete()
 
-    
     def test_save_location(self):
         self.new_location.save_location()
         location = Location.objects.all()
         self.assertTrue(len(location) > 0)
 
-    
     def test_delete_location(self):
         self.new_location.save_location()
         self.new_location.delete_location()
         location = Location.objects.all()
         self.assertEqual(len(location), 0)
 
-    
     def test_update_location(self):
         self.new_location.save_location()
-        updated_location = Location.update_location(self.new_location.id, 'Mombasa')
+        updated_location = Location.update_location(
+            self.new_location.id, 'Mombasa')
         self.assertEqual(updated_location.location, 'Mombasa')
 
 
@@ -66,27 +65,25 @@ class ImageTestClass(TestCase):
         # creating a new location and saving it
         self.new_location = Location(location='Nairobi')
         self.new_location.save()
-        
-        #creating a new category and saving it
+
+        # creating a new category and saving it
         self.new_category = Category(category='nature')
         self.new_category.save()
 
-        #creating a new image and saving it
-        self.new_image = Image( name = "birthday21", description = "Turns 21", image_pic ="photo.jpeg", img_category = self.new_category, img_location = self.new_location)
+        # creating a new image and saving it
+        self.new_image = Image(name="birthday21", description="Turns 21", image_pic="photo.jpeg",
+                               img_category=self.new_category, img_location=self.new_location)
         self.new_image.save_image()
-
 
     def tearDown(self):
         Image.objects.all().delete()
         Category.objects.all().delete()
         Location.objects.all().delete()
 
-    
     def test_instance(self):
         self.assertTrue(isinstance(self.new_image, Image))
         self.assertTrue(isinstance(self.new_location, Location))
         self.assertTrue(isinstance(self.new_category, Category))
-
 
     def test_save_image(self):
         self.new_image.save_image()
@@ -95,24 +92,25 @@ class ImageTestClass(TestCase):
         locations = Location.objects.all()
         categories = Category.objects.all()
         images = Image.objects.all()
-        self.assertTrue(len(images)> 0)
-        self.assertTrue(len(categories)> 0)
-        self.assertTrue(len(locations)> 0)
+        self.assertTrue(len(images) > 0)
+        self.assertTrue(len(categories) > 0)
+        self.assertTrue(len(locations) > 0)
 
     def test_filter_by_location(self):
         self.new_image.save_image()
-        img_location = Image.filter_by_location(self.new_image.img_location == img_location)
-        self.assertEqual(img_location.location, "Nairobi" )
+        img_location = Image.filter_by_location(
+            self.new_image.img_location == img_location)
+        self.assertEqual(img_location.location, "Nairobi")
 
     def test_get_image_by_id(self):
         self.new_image.save_image()
         image = Image.get_image_by_id(self.new_image.id)
         self.assertEqual(self.new_image.name, image.name)
 
-
     def test_search_image_by_category(self):
         self.new_image.save_image()
-        image_category = Image.search_image_by_category(self.new_image.img_category.category)
+        image_category = Image.search_image_by_category(
+            self.new_image.img_category.category)
         self.assertEqual(image_category.img_category.category, 'nature')
 
     def test_update_image(self):
